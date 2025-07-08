d4b864ad4d99bde1cd6c56609ec8521db24e92af
adakasriman <adakasriyadav>
2025-07-08 10:05:52 +0530

    changes in logout api

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -12,7 +12,7 @@ export interface LoginResponse {
 
 export const login = async (payload: LoginPayload): Promise<LoginResponse> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.post<LoginResponse>(API_URL + '/api/login', payload,{
+    const { data } = await axios.post<LoginResponse>(API_URL + '/api/login', payload, {
         withCredentials: true
     });
     return data;
@@ -20,31 +20,31 @@ export const login = async (payload: LoginPayload): Promise<LoginResponse> => {
 
 export const logout = async (): Promise<any> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.post<any>(API_URL + '/api/logout', {
-        withCredentials: true
-    });
+    const { data } = await axios.post<any>(
+        API_URL + '/api/logout',
+        {},
+        { withCredentials: true }
+    );
     return data;
 };
-
-
 export const signup = async (payload: LoginPayload): Promise<LoginResponse> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.post<LoginResponse>(API_URL + '/api/signup', payload,{
+    const { data } = await axios.post<LoginResponse>(API_URL + '/api/signup', payload, {
         withCredentials: true
     });
     return data;
 };
 export const userList = async (): Promise<any> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.get<any>(API_URL + '/api/users',{
+    const { data } = await axios.get<any>(API_URL + '/api/users', {
         withCredentials: true
-    });   
+    });
     return data;
 };
 
 export const updatePassword = async (payload: any): Promise<any> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.put<any>(API_URL + '/api/update-password', payload,{
+    const { data } = await axios.put<any>(API_URL + '/api/update-password', payload, {
         withCredentials: true
     });
     return data;
@@ -52,7 +52,7 @@ export const updatePassword = async (payload: any): Promise<any> => {
 
 export const deleteUser = async (id: number): Promise<any> => {
     const API_URL = import.meta.env.VITE_API_URL;
-    const { data } = await axios.delete<any>(API_URL + '/api/users/' + id,{
+    const { data } = await axios.delete<any>(API_URL + '/api/users/' + id, {
         withCredentials: true
     });
     return data;
